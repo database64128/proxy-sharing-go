1098a3f03af834f5ce3d3188c9d5d468969265aa
database64128 <free122448@hotmail.com>
2024-12-04 11:28:13 +0800

    📮 cmd: add version flag
    Log name and version on startup.

--- cmd/proxy-sharing-go/main.go
+++ cmd/proxy-sharing-go/main.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"runtime/debug"
 	"syscall"
 
+	proxysharinggo "github.com/database64128/proxy-sharing-go"
 	"github.com/database64128/proxy-sharing-go/jsonhelper"
 	"github.com/database64128/proxy-sharing-go/logging"
 	"github.com/database64128/proxy-sharing-go/service"
@@ -16,6 +18,7 @@ import (
 )
 
 var (
+	version  bool
 	testConf bool
 	confPath string
 	zapConf  string
@@ -23,6 +26,7 @@ var (
 )
 
 func init() {
+	flag.BoolVar(&version, "version", false, "Print the version and exit")
 	flag.BoolVar(&testConf, "testConf", false, "Test the configuration file and exit without starting the services")
 	flag.StringVar(&confPath, "confPath", "config.json", "Path to the JSON configuration file")
 	flag.StringVar(&zapConf, "zapConf", "console", "Preset name or path to the JSON configuration file for building the zap logger.\nAvailable presets: console, console-nocolor, console-notime, systemd, production, development")
@@ -32,12 +36,22 @@ func init() {
 func main() {
 	flag.Parse()
 
+	if version {
+		os.Stdout.WriteString("proxy-sharing-go " + proxysharinggo.Version + "\n")
+		if info, ok := debug.ReadBuildInfo(); ok {
+			os.Stdout.WriteString(info.String())
+		}
+		return
+	}
+
 	logger, err := logging.NewZapLogger(zapConf, logLevel)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to build logger:", err)
 		os.Exit(1)
 	}
 	defer logger.Sync()
+
+	logger.Info("proxy-sharing-go", zap.String("version", proxysharinggo.Version))
 
 	var sc service.Config
 	if err = jsonhelper.OpenAndDecodeDisallowUnknownFields(confPath, &sc); err != nil {
---
+++ version.go
@@ -0,0 +1,4 @@
+package proxysharinggo
+
+// Version is the current version of proxy-sharing-go.
+const Version = "0.1.0"
